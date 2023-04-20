2da005aed529f2f52a111ff1476fc5717ad9814e
Arjun Santhosh <arjunsanthosh738@gmail.com>
2023-04-20 17:32:41 +0530

    Commit 11 Backend

--- DB/db.js
+++ DB/db.js
@@ -8,7 +8,7 @@ module.exports = () => {
     }
 
     try {
-        mongoose.connect(process.env.MONGO_URI, connectionParams)
+        mongoose.connect("mongodb+srv://Arjun_Santhosh:VJQmP0w2j0rezS19@redwings.ngtzzdg.mongodb.net/RedWings", connectionParams)
         console.log('Connected to database successfully.....');
     } catch (error) {
         console.log(error);
